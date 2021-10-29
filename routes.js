33a63f4d748d876aa11d8c1ffbc62868d7de8e20
luizfernandorg <luizfernandorg@gmail.com>
2021-10-29 05:23:34 -0300

    defined better name for variable

--- routes.js
+++ routes.js
@@ -9,13 +9,13 @@ module.exports = function (app, myDataBase) {
     app.post("/api/shorturl", (req,res) => {
 
         const cursor = myDataBase.find({});
-        cursor.toArray().then(ele => {
-            if(ele.length) {
-                const result = myDataBase.insertOne({'short_url':ele.length+1,"original_url":req.body.url});
-                res.json({'short_url':ele.length+1,'original_url':req.body.url});      
+        cursor.toArray().then(elements => {
+            if(elements.length) {
+                const result = myDataBase.insertOne({'short_url':elements.length+1,"original_url":req.body.url});
+                res.json({'original_url':req.body.url,'short_url':elements.length+1});      
             } else {
                 const result = myDataBase.insertOne({'short_url':1,"original_url":req.body.url});
-                res.json({'short_url':1,'original_url':req.body.url});           
+                res.json({'original_url':req.body.url,'short_url':1});           
             }
         });
     });
@@ -23,8 +23,8 @@ module.exports = function (app, myDataBase) {
     app.get('/api/shorturl/:id', (req,res) => {
         // findone in db by index (1,2,3...)
         const result = myDataBase.findOne({"short_url": req.params.id})
-        
+        res.redirect(result.original_url);
         // Example of output: { "short_url": 2, "original_url": "url"}
-        res.json({'short_url':result.short_url,"original_url":result.original_url});
+        //res.json({'short_url':result.short_url,"original_url":result.original_url});
     })
   }
