8f1a213d90b9162fab67c914c95f12fad8c7c779
luizfernandorg <luizfernandorg@gmail.com>
2021-10-31 01:25:06 -0300

    update RegEx and removed dns.lookup

--- routes.js
+++ routes.js
@@ -8,32 +8,39 @@ module.exports = function (app, UrlModel) {
     })
     
     app.post("/api/shorturl", (req,res) => {
-      var url = /^((http|ftp|https):\/\/|((www|WWW)\.)){1}?([a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+(\.[a-zA-Z0-9]+)*)$/;
+      console.log(req.body.url);
+      var url = /^(http|https|ftp):\/\/[a-zA-Z0-9]*\.([a-zA-Z0-9]*\.){1,3}[a-zA-Z0-9]{2,3}\/*\?*[a-zA-Z0-9]*=[a-zA-Z0-9]*$/;
       var regex = new RegExp(url);
       if(!req.body.url.match(regex)){
         res.json({ error: 'invalid url' })
         return;
       }else{
-        
-        UrlModel.findOne({}).sort({short_url: -1}).then( data => {
-            if(data == undefined){
-              const newUrl = new UrlModel({
-                original_url:req.body.url,
-                short_url:1
-              });
-              newUrl.save().then((err,doc)=>{
-                if(err) console.log(err);
-                res.json({'original_url': doc.original_url,'short_url':doc.short_url});
-              });
-            }else{
-              UrlModel.findOneAndUpdate({original_url:req.body.url},{original_url:req.body.url,short_url:data.short_url+1},{
-                new: true,
-                upsert: true // Make this update into an upsert
-              },(err,doc) => {
-                res.json({'original_url':doc.original_url,'short_url':doc.short_url})
-              });
-            }            
-        });
+       /* dns.lookup(req.body.url.split("://")[1],(err,address,family) => {
+          if(err){
+            res.json({ error: 'invalid url' })
+            return console.log(address)
+          }else{*/
+            UrlModel.findOne({}).sort({short_url: -1}).then( data => {
+              if(data == undefined){
+                const newUrl = new UrlModel({
+                  original_url:req.body.url,
+                  short_url:1
+                });
+                newUrl.save().then((err,doc)=>{
+                  if(err) console.log(err);
+                  res.json({'original_url': doc.original_url,'short_url':doc.short_url});
+                });
+              }else{
+                UrlModel.findOneAndUpdate({original_url:req.body.url},{original_url:req.body.url,short_url:data.short_url+1},{
+                  new: true,
+                  upsert: true // Make this update into an upsert
+                },(err,doc) => {
+                  res.json({'original_url':doc.original_url,'short_url':doc.short_url})
+                });
+              }            
+            });
+          /*}
+       // });*/
       }
     });
 
