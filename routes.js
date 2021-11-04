32b312d4f38d606ab2eb8a3f49a705435ba14fdd
luizfernandorg <luizfernandorg@gmail.com>
2021-11-03 22:25:51 -0300

    Update route to insert or update an element

--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const dns = require('dns');
+const { url } = require('url');
 
 module.exports = function (app, myDataBase) {
 
@@ -7,18 +9,46 @@ module.exports = function (app, myDataBase) {
     })
     
     app.post("/api/shorturl", (req,res) => {
-        //TODO get the highest shorturl to increment by 1 but also certify if that already exist an equal url
-        const cursor = myDataBase.find({}).sort({short_url:-1}).toArray((err, elements) => {
-            console.log(elements[0].short_url)
-            res.send("Testing input");
-            if(elements.length) {
-                const result = myDataBase.insertOne({'short_url':elements[0].short_url+1,"original_url":req.body.url});
-                res.json({'original_url':req.body.url,'short_url':elements[0].short_url+1});      
-            } else {
-                const result = myDataBase.insertOne({'short_url':1,"original_url":req.body.url});
-                res.json({'original_url':req.body.url,'short_url':1});           
-            }
-        });
+
+        const urlRegex = /^(http|https|ftp):\/\/([a-zA-Z0-9\-]*\.)*([a-zA-Z0-9]*\.){1,3}[a-zA-Z0-9]{2,5}\/*(\?*[a-zA-Z0-9]*=[a-zA-Z0-9]*)*$/;
+        const regex = new RegExp(urlRegex);
+        if(!req.body.url.match(regex)){
+            res.json({ error: 'invalid url' })
+            return;
+        }else{
+            const hostname = new URL(req.body.url).hostname
+            dns.lookup(hostname,(err,address,family) => {
+                if(err){
+                    res.json({ error: 'invalid url' })
+                    return console.log(address)
+                }else{
+                    myDataBase.find({}).sort({short_url:-1}).toArray((err,elements) => {
+                        if(err){
+                            return console.log(address)
+                        }else{
+                            if(elements === undefined){
+                                const result = myDataBase.insertOne({'short_url':1,"original_url":req.body.url});
+                                res.json({'original_url':req.body.url,'short_url':1});
+                            } else {
+                                //update the existed url
+                                myDataBase.updateOne({original_url: req.body.url}, { $set: {short_url: elements[0].short_url+1}}).then((doc) => {
+                                    // if the url exist and was modified find the url and respond with the json
+                                    if(doc.modifiedCount) {
+                                        myDataBase.find({original_url: req.body.url}).toArray( (err,element) => {
+                                            res.json({'original_url':element[0].original_url,'short_url':element[0].short_url})
+                                        });
+                                    } else {
+                                        //if the url doesn't exist insert it
+                                        myDataBase.insertOne({'short_url':elements[0].short_url+1,"original_url":req.body.url});
+                                        res.json({'original_url':req.body.url,'short_url':elements[0].short_url+1});  
+                                    }
+                                })
+                            }
+                        }
+                    })                    
+                }
+            });
+        }
     });
 
     app.get('/api/shorturl/:id', (req,res) => {
